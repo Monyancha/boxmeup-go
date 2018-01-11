3b2e14f293647ba598d42e58ba0e2a24006327b2
Chris Saylor <cjsaylor@gmail.com>
2018-01-11 13:53:55 -0500

    Add method to extract users from items.

--- modules/items/item.go
+++ modules/items/item.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/cjsaylor/boxmeup-go/modules/containers"
+	"github.com/cjsaylor/boxmeup-go/modules/users"
 )
 
 // ContainerItem represents a single item in a container
@@ -20,6 +21,20 @@ type ContainerItem struct {
 // ContainerItems is a collection of container items.
 type ContainerItems []ContainerItem
 
+// ExtractUsers from arbitrary container items
+func (i *ContainerItems) ExtractUsers() []users.User {
+	ids := make(map[int64]bool)
+	users := make([]users.User, 0)
+	for _, item := range *i {
+		if !ids[item.Container.User.ID] {
+			ids[item.Container.User.ID] = true
+			users = append(users, item.Container.User)
+		}
+	}
+	return users
+}
+
+// ExtractContainers from arbitrary container items
 func (i *ContainerItems) ExtractContainers() []containers.Container {
 	ids := make(map[int64]bool)
 	containers := make([]containers.Container, 0)
