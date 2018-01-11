055aa4e4fb4d7ecad729593c3a5fc1583ccbc6d9
Chris Saylor <cjsaylor@gmail.com>
2018-01-11 14:13:55 -0500

    Added JsonErrorResponse from handlers to middleware.
    This is so that it can be shared to the hooks.

--- modules/middleware/json.go
+++ modules/middleware/json.go
@@ -2,6 +2,11 @@ package middleware
 
 import "net/http"
 
+type JsonErrorResponse struct {
+	Code int    `json:"code"`
+	Text string `json:"text"`
+}
+
 func JsonResponseHandler(next http.Handler) http.Handler {
 	fn := func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "application/json; charset=UTF-8")
