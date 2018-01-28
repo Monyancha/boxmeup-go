e0794fbdda4de880a95f8112c3eef578c2c91771
Chris Saylor <cjsaylor@gmail.com>
2018-01-28 16:24:56 -0500

    Add container to return view.

--- modules/items/item.go
+++ modules/items/item.go
@@ -10,7 +10,7 @@ import (
 // ContainerItem represents a single item in a container
 type ContainerItem struct {
 	ID        int64                 `json:"id"`
-	Container *containers.Container `json:"-"`
+	Container *containers.Container `json:"container"`
 	UUID      string                `json:"uuid"`
 	Body      string                `json:"body"`
 	Quantity  int                   `json:"quantity"`
